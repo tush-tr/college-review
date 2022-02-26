095862875ad693c741c7687460d4234d3e5b060d
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-02-26 08:14:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 gcloud container clusters create crk --zone=us-west1-a
 sleep 2
 kubectl apply -f pod.yaml
+kubectl expose pods college-review-pod1 --port=80 --type=LoadBalancer
 kubectl get services
