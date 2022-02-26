8e0422e1eff40e8ce31299540a936d7cb936ab1d
tush-tr <tusharrajput604@gmail.com>
2022-02-27 04:46:51 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const setupMiddlewares = require("./utils/middlewares");
 const app = express();
 
-const port = 3000 | process.env.PORT;
+const port = 3000 || process.env.PORT;
+var server_host = 'localhost' || '0.0.0.0';
 
 setupMiddlewares(express, app);
 
@@ -11,6 +12,6 @@ const reviewsRouter = require("./routes/reviews");
 app.use("/", homeRouter);
 app.use("/", reviewsRouter);
 
-app.listen(port, () => {
+app.listen(port,server_host, () => {
   console.log(`The application is running on the port ${port}`);
 });
