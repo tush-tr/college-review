517050f9b33289784f9c12d56ba03350725e6dab
tush-tr <tusharrajput604@gmail.com>
2022-02-26 07:34:08 +0530

    some stylings

--- views/collegebox.jsx
+++ views/collegebox.jsx
@@ -2,24 +2,24 @@ const React = require("react");
 
 const CollegeBox = ({ collegeName, collegeReview, key }) => {
   return (
-    <tr>
+    <tr className="college-review-table-row">
       <td className="collegename">
         <b>College Name: </b>
         {collegeName}
       </td>
-      <td className="reviewof">
+      <td className="reviewof review-box-wrapper">
         <h4>Review: </h4>
         <br />
-        <h6>Course Curriculum</h6>
-        <p>{collegeReview["Course Curriculum"]}</p>
-        <h6>About faculty</h6>
-        <p>{collegeReview["About faculty"]}</p>
-        <h6>Campus Life</h6>
-        <p>{collegeReview["Campus Life"]}</p>
-        <h6>Scholarship</h6>
-        <p>{collegeReview["Scholarship"]}</p>
-        <h6>Placement</h6>
-        <p>{collegeReview["Placement"]}</p>
+        <h6 className="review-heading-title">Course Curriculum</h6>
+        <p className="review-heading-paragraph">{collegeReview["Course Curriculum"]}</p>
+        <h6 className="review-heading-title">About faculty</h6>
+        <p className="review-heading-paragraph">{collegeReview["About faculty"]}</p>
+        <h6 className="review-heading-title">Campus Life</h6>
+        <p className="review-heading-paragraph">{collegeReview["Campus Life"]}</p>
+        <h6 className="review-heading-title">Scholarship</h6>
+        <p className="review-heading-paragraph">{collegeReview["Scholarship"]}</p>
+        <h6 className="review-heading-title">Placement</h6>
+        <p className="review-heading-paragraph">{collegeReview["Placement"]}</p>
       </td>
     </tr>
   );
